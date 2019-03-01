404f31f81dafbad452fa0d138c0565d20d27140d
kuldeep sharma <48021207+kuldeep16896@users.noreply.github.com>
2019-03-01 21:14:49 +0530

    4th day code
    find even or odd number

---
+++ evenoddnumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the value of n:");
+scanf("%d",&n);
+if(n%2==0)
+printf("number is a even number:");
+else
+printf("number is a odd number:");
+}
