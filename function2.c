758d81de622a1c700539c9148e8c72b76f88d3ce
kuldeep sharma <48021207+kuldeep16896@users.noreply.github.com>
2019-03-09 00:16:39 +0530

    functionexample.c
    add to number using function statement

---
+++ function2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+void sum();  
+void main()  
+{  
+    printf("\nGoing to calculate the sum of two numbers:");  
+    sum();  
+}  
+void sum()  
+{  
+    int a,b;   
+    printf("\nEnter two numbers");  
+    scanf("%d %d",&a,&b);   
+    printf("The sum is %d",a+b);  
